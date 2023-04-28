2b9fa29ed144040580d1646f6d23dbe5ef2ef8b6
fadyjan <fady.jan95@gmail.com>
2023-04-28 05:15:54 +0200

    Create #3 Loading Spinner Component

---
+++ src/components/LoadingSpinner/LoadingSpinner.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import PulseLoader from "react-spinners/PropagateLoader";
+
+const LoadingSpinner = () => {
+  return (
+<PulseLoader color="#36d7b7" />
+  )
+}
+
+export default LoadingSpinner
